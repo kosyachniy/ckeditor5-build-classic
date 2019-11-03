ec6ec43b638366f3c2831a0bf7630a0c2a35d628
kosyachniy <polozhev@mail.ru>
2019-11-03 15:13:31 +0300

    Module with formulas

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
+import MathType from '@wiris/mathtype-ckeditor5/src/plugin';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -54,7 +56,9 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+
+	MathType,
 ];
 
 // Editor configuration.
@@ -77,7 +81,10 @@ ClassicEditor.defaultConfig = {
 			'insertTable',
 			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+			'|',
+			'MathType',
+			'ChemType',
 		]
 	},
 	image: {
